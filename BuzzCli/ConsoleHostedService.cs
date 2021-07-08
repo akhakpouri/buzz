541a33342322dc36cdd7dd403615247db88250f3
Ali Khakpouri <ali.khakpouri@gmail.com>
2021-07-08 16:04:46 -0400

    Initial commit

---
+++ BuzzCli/ConsoleHostedService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BuzzCli
+{
+    internal sealed class ConsoleHostedService : IHostedService
+    {
+        readonly ILogger _logger;
+        readonly IHostApplicationLifetime _appLifetime;
+        int? _exitCode;
+
+        public ConsoleHostedService(
+            ILogger<ConsoleHostedService> logger,
+            IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+
+            _appLifetime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(async () =>
+                {
+                    try
+                    {
+                        _logger.LogInformation("Hello World!");
+                        await Task.Delay(1000);
+                        _exitCode = 0;
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Unhandled exception!");
+                        _exitCode = 1;
+                    }
+                    finally
+                    {
+                        _appLifetime.StopApplication();
+                    }
+                });
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogDebug($"Exiting with return code: {_exitCode}");
+            Environment.ExitCode = _exitCode.GetValueOrDefault(-1);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BuzzCli/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace BuzzCli
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+            return host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+           .ConfigureServices((_, services) =>
+           {
+               services
+                .AddLogging()
+                .AddHostedService<ConsoleHostedService>();               
+           });
+    }
+
+}
